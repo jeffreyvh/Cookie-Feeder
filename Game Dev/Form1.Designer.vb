f727f16db7f2637d047483dbcd37977962d2c557
jeffreyvh <jeffrey25011998@gmail.com>
2015-06-01 08:15:33 +0200

    Release
    Initial Release

---
+++ Game Dev/Form1.Designer.vb
@@ -0,0 +1,308 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.statuscookie = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.BonusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ScoreTotal = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Store = New System.Windows.Forms.Panel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TabPage4 = New System.Windows.Forms.TabPage()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.BonusLabelTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.AutoFeed1 = New System.Windows.Forms.Timer(Me.components)
+        Me.StatusStrip1.SuspendLayout()
+        Me.Store.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.statuscookie, Me.BonusLabel})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 450)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(394, 22)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(111, 17)
+        Me.ToolStripStatusLabel1.Text = "Cookie per second :"
+        '
+        'statuscookie
+        '
+        Me.statuscookie.Name = "statuscookie"
+        Me.statuscookie.Size = New System.Drawing.Size(13, 17)
+        Me.statuscookie.Text = "0"
+        '
+        'BonusLabel
+        '
+        Me.BonusLabel.Name = "BonusLabel"
+        Me.BonusLabel.Size = New System.Drawing.Size(49, 17)
+        Me.BonusLabel.Text = "Bonus..."
+        Me.BonusLabel.Visible = False
+        '
+        'ScoreTotal
+        '
+        Me.ScoreTotal.AutoSize = True
+        Me.ScoreTotal.Font = New System.Drawing.Font("28 Days Later", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ScoreTotal.Location = New System.Drawing.Point(146, 28)
+        Me.ScoreTotal.Name = "ScoreTotal"
+        Me.ScoreTotal.Size = New System.Drawing.Size(25, 25)
+        Me.ScoreTotal.TabIndex = 4
+        Me.ScoreTotal.Text = "0"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Store
+        '
+        Me.Store.Controls.Add(Me.GroupBox1)
+        Me.Store.Location = New System.Drawing.Point(397, 34)
+        Me.Store.Name = "Store"
+        Me.Store.Size = New System.Drawing.Size(370, 438)
+        Me.Store.TabIndex = 6
+        Me.Store.Visible = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TabControl1)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(370, 438)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "STORE"
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Controls.Add(Me.TabPage4)
+        Me.TabControl1.Location = New System.Drawing.Point(6, 19)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(358, 407)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(350, 381)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Animals"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(350, 381)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Cookies"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.Button1)
+        Me.TabPage3.Controls.Add(Me.Label2)
+        Me.TabPage3.Controls.Add(Me.Label1)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(350, 381)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Engine"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(261, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "BUY : 50"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(-76, 34)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(566, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "=============================================================="
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(3, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(141, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "AUTO FEED : 1 sec"
+        '
+        'TabPage4
+        '
+        Me.TabPage4.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage4.Name = "TabPage4"
+        Me.TabPage4.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage4.Size = New System.Drawing.Size(350, 381)
+        Me.TabPage4.TabIndex = 3
+        Me.TabPage4.Text = "Exit Store"
+        Me.TabPage4.UseVisualStyleBackColor = True
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.Game_Dev.My.Resources.Resources.poop
+        Me.PictureBox4.Location = New System.Drawing.Point(175, 201)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(50, 50)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 7
+        Me.PictureBox4.TabStop = False
+        Me.PictureBox4.Visible = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.Game_Dev.My.Resources.Resources.Store_Button
+        Me.PictureBox3.Location = New System.Drawing.Point(13, 13)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(125, 50)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 5
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox2.BackgroundImage = Global.Game_Dev.My.Resources.Resources.Cookie1
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox2.Location = New System.Drawing.Point(175, 397)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(50, 50)
+        Me.PictureBox2.TabIndex = 3
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = Global.Game_Dev.My.Resources.Resources.Animal1
+        Me.PictureBox1.Location = New System.Drawing.Point(151, 275)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 100)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'BonusLabelTimer
+        '
+        Me.BonusLabelTimer.Interval = 2000
+        '
+        'AutoFeed1
+        '
+        Me.AutoFeed1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(394, 472)
+        Me.Controls.Add(Me.Store)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.ScoreTotal)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.Store.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage3.ResumeLayout(False)
+        Me.TabPage3.PerformLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents statuscookie As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents PictureBox2 As System.Windows.Forms.Panel
+    Friend WithEvents ScoreTotal As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents Store As System.Windows.Forms.Panel
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents BonusLabel As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents BonusLabelTimer As System.Windows.Forms.Timer
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage3 As System.Windows.Forms.TabPage
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TabPage4 As System.Windows.Forms.TabPage
+    Friend WithEvents AutoFeed1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Game Dev/Form1.vb
@@ -0,0 +1,130 @@
+﻿Public Class Form1
+
+    Dim cookiex, cookiey As Integer
+    Dim totallocation As System.Drawing.Point
+    Dim cookieinsec As Double = 0
+
+    Dim autofeedone As Boolean = False
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        save()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim storeloc As System.Drawing.Point
+        storeloc.X = 12
+        storeloc.Y = 12
+        Store.Location = storeloc
+        Dim title As String = Game_Dev.My.Application.Info.ProductName.ToString
+        Me.Text = title
+        Timer1.Start()
+        AutoFeed1.Start()
+
+        ''here comes the savedata load process
+    End Sub
+
+    Private Sub PictureBox2_MouseDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox2.MouseDown
+        cookiex = PictureBox2.Location.X
+
+        ''neccessary
+        cookiey = Control.MousePosition.Y - PictureBox2.Location.Y
+    End Sub
+
+    Private Sub PictureBox2_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox2.MouseMove
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            totallocation.X = cookiex
+
+            totallocation.Y = Control.MousePosition.Y - Me.Top
+            PictureBox2.Location = totallocation
+        End If
+
+        ''Check for the Cookie to reach 50px difference
+        If PictureBox2.Location.Y <= 397 - 50 Then
+            PictureBox2.Enabled = False
+            Dim locationdef As System.Drawing.Point
+            locationdef.Y = 397
+            locationdef.X = 175
+            PictureBox2.Location = locationdef
+            Dim score As Integer = ScoreTotal.Text + 1
+            ScoreTotal.Text = score
+            cookieinsec += 1
+            PictureBox2.Enabled = True
+
+            ''make the animal bigger...
+            If Not PictureBox1.Size.Width >= 150 Then
+                PictureBox1.Size = New Size(PictureBox1.Size.Width + 2, PictureBox1.Size.Height + 2)
+                Dim locationpoint As System.Drawing.Point
+                locationpoint.X = PictureBox1.Location.X - 1
+                locationpoint.Y = PictureBox1.Location.Y - 1
+                PictureBox1.Location = locationpoint
+            Else
+                PictureBox4.Visible = True
+                Dim locationpoint As System.Drawing.Point
+                locationpoint.X = PictureBox1.Location.X + 25
+                locationpoint.Y = PictureBox1.Location.Y + 25
+                PictureBox1.Location = locationpoint
+                PictureBox1.Size = New Size(PictureBox1.Size.Width - 50, PictureBox1.Size.Height - 50)
+            End If
+        End If
+    End Sub
+
+    Private Sub PictureBox2_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox2.MouseUp
+        Dim locationdef As System.Drawing.Point
+        locationdef.Y = 397
+        locationdef.X = 175
+        PictureBox2.Location = locationdef
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        statuscookie.Text = cookieinsec
+        cookieinsec = 0
+    End Sub
+
+    Private Sub TabControl1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TabControl1.SelectedIndexChanged
+        If TabControl1.SelectedTab.Text = "Exit Store" Then
+            Store.Visible = False
+            TabControl1.SelectedTab = TabControl1.TabPages(0)
+        End If
+    End Sub
+
+    Private Sub PictureBox3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox3.Click
+        Store.Visible = True
+    End Sub
+
+    Private Sub PictureBox4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox4.Click
+        PictureBox4.Visible = False
+        Dim score As Integer = ScoreTotal.Text + 10
+        ScoreTotal.Text = score
+        BonusLabel.Text = "Poop Bonus : + 10"
+        BonusLabel.Visible = True
+        BonusLabelTimer.Start()
+    End Sub
+
+    Private Sub BonusLabelTimer_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BonusLabelTimer.Tick
+        BonusLabel.Visible = False
+    End Sub
+
+    Private Sub AutoFeed1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AutoFeed1.Tick
+        If autofeedone = True Then
+            Dim score As Integer = ScoreTotal.Text + 1
+            ScoreTotal.Text = score
+            cookieinsec += 1
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim score As Double = ScoreTotal.Text
+        If score >= 49 Then
+            Dim scorenew As Double = score - 50
+            ScoreTotal.Text = scorenew
+            Button1.Enabled = False
+            autofeedone = True
+        Else
+            MsgBox("Sorry, you don't have enought points yet.")
+        End If
+    End Sub
+
+    Public Sub save()
+        Dim scorefile As String = Application.StartupPath.ToString & "\SaveData\score.dat"
+    End Sub
+End Class
